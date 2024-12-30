ba7d77801108586e63959b6e0b9d6c2c9e8320cc
Oky Aneka <okyaneka@gmail.com>
2024-12-30 13:54:52 +0700

    feat: Refactor StageRoute to use Joi validation schemas from StageValidator

---
+++ src/validators/StageValidator/index.ts
@@ -0,0 +1,27 @@
+import schema from "~/helpers/schema";
+import { DefaultListParams, DefaultListParamsFields } from "..";
+import Joi from "joi";
+
+export interface StageListParams extends DefaultListParams {}
+
+export interface StageCreatePayload {
+  name: string;
+  storyline: string[];
+}
+
+export interface StageUpdatePayload {
+  name: string;
+  storyline: string[];
+}
+
+export const StageListParamsSchema = schema.generate<StageListParams>({
+  ...DefaultListParamsFields,
+});
+
+export const StageCreatePayload = schema.generate<StageCreatePayload>({
+  name: Joi.string().required(),
+  storyline: Joi.array().items(Joi.string()).default([]),
+});
+
+export const StageUpdatePayload =
+  StageCreatePayload as Joi.ObjectSchema<StageUpdatePayload>;
