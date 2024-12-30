f692f4b0c9e10ad7617e00e6289058e8ec7a2254
Oky Aneka <okyaneka@gmail.com>
2024-12-30 11:18:26 +0700

    feat: Implement Qr model with Mongoose schema and IdName interface

---
+++ src/models/Qr.ts
@@ -0,0 +1,49 @@
+import mongoose, { ToObjectOptions } from "mongoose";
+import { IdName, idNameSchema } from ".";
+import CryptoJS from "crypto-js";
+
+export interface Qr {
+  id: string;
+  code: string;
+  stage: IdName | null;
+  challenge: IdName | null;
+  isUsed: boolean;
+  accessCount: number | null;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+}
+
+const toObjectOptions: ToObjectOptions = {
+  transform: (doc, ret) => {
+    const { _id, deletedAt, ...rest } = ret;
+    return { id: _id, ...rest };
+  },
+};
+
+const qrSchema = new mongoose.Schema<Qr>(
+  {
+    code: { type: String, default: "" },
+    stage: { type: idNameSchema, default: null },
+    challenge: { type: idNameSchema, default: null },
+    isUsed: { type: Boolean, default: false },
+    accessCount: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    deletedAt: { type: Date, default: null },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+qrSchema.virtual("id").get(function () {
+  return this._id.toString();
+});
+qrSchema.set("toObject", toObjectOptions);
+qrSchema.set("toJSON", toObjectOptions);
+
+export const Qr = mongoose.model<Qr>("Qr", qrSchema);
+
+export default Qr;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,14 @@
+import { Schema } from "mongoose";
+
 export { default as User } from "./User";
 export { default as Stage } from "./Stage";
+
+export interface IdName {
+  id: string;
+  name: string;
+}
+
+export const idNameSchema = new Schema<IdName>({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+});
