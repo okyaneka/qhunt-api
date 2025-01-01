135c01545bd7a6efb944ff6dd854d5f63e2ac4d5
Oky Aneka <okyaneka@gmail.com>
2025-01-01 16:22:34 +0700

    feat: add Trivia model and validation schema with CRUD service

--- src/helpers/schema.ts
+++ src/helpers/schema.ts
@@ -1,6 +1,44 @@
 import Joi from "joi";
 import mongoose from "mongoose";
 
+interface ValidatorOption<T = unknown> {
+  required?: boolean;
+  defaultValue?: T;
+  allow?: T;
+}
+
+const creaetValidator = <T = unknown>(
+  base: Joi.Schema<T> | Joi.ArraySchema<T>,
+  option?: ValidatorOption<T>
+): Joi.Schema<T> | Joi.ArraySchema<T> => {
+  let v = base;
+  if (option?.required) v = v.required();
+  if (option?.allow != undefined) v = v.allow(option.allow);
+  if (option?.defaultValue) v = v.default(option.defaultValue);
+  return v;
+};
+
+export const string = (option?: ValidatorOption<string>) =>
+  creaetValidator<string>(Joi.string(), option);
+
+export const number = (option?: ValidatorOption<number>) =>
+  creaetValidator<number>(Joi.number(), option);
+
+export const boolean = (option?: ValidatorOption<boolean>) =>
+  creaetValidator<boolean>(Joi.boolean(), option);
+
+export const array = <T = unknown>(
+  item: Joi.Schema<T>,
+  options?: ValidatorOption<T>
+) => {
+  let v = creaetValidator<T>(
+    Joi.array<T>().items(item),
+    options
+  ) as Joi.ArraySchema<T>;
+  if (options?.required) v = v.min(1);
+  return v;
+};
+
 export const generate = <T>(
   fields: Record<keyof T, Joi.Schema>
 ): Joi.ObjectSchema<T> => Joi.object(fields);
@@ -12,6 +50,6 @@ export const ToObject: mongoose.ToObjectOptions = {
   },
 };
 
-const schema = { generate };
+const schema = { string, number, boolean, array, generate, ToObject };
 
 export default schema;
