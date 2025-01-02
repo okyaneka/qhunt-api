a1126b7621b1abcd582c6727a50d14eb38998a65
Oky Aneka <okyaneka@gmail.com>
2025-01-02 20:14:13 +0700

    feat: enhance AuthMiddleware to use UserService for user retrieval and improve error handling

--- src/middlewares/AuthMiddleware/index.ts
+++ src/middlewares/AuthMiddleware/index.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import response from "~/helpers/response";
-import jwt from "jsonwebtoken";
+import { decode, verify } from "jsonwebtoken";
 import { ENV } from "~/configs";
-import User from "~/models/User";
+import UserService from "~/services/UserService";
 
 const AuthMiddleware: RequestHandler = async (
   req: Request,
@@ -11,26 +11,27 @@ const AuthMiddleware: RequestHandler = async (
 ) => {
   const bearer = req.headers.authorization;
   if (!bearer || !bearer.startsWith("Bearer ")) {
-    res.status(401).json(response.error({}, "Unauthorized", 401));
+    res.status(401).json(response.error({}, "unauthorized", 401));
     return;
   }
 
   const token = bearer?.split(" ")[1];
 
-  jwt.verify(token, ENV.JWT_SECRET, (err) => {
+  verify(token, ENV.JWT_SECRET, (err) => {
     if (err) {
       res.status(401).json(response.error({}, err.message, 401));
       return;
     }
   });
 
-  const decoded = jwt.decode(token) as { id: string };
-  const user = await User.findById(decoded.id);
-  if (!user) {
-    res.status(401).json(response.error({}, "Invalid auth", 401));
+  const { id } = decode(token) as { id: string };
+  const user = await UserService.detail(id);
+  if (!user || user instanceof Error) {
+    res.status(401).json(response.error({}, "invalid auth", 401));
     return;
   }
-  res.locals.user = user;
+
+  res.locals.user = { id: user._id, role: user.role };
 
   next();
 };
