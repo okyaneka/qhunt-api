eac51547f1098ed349fd17b4fedb06dd1905d1be
Oky Aneka <okyaneka@gmail.com>
2025-01-02 20:13:28 +0700

    feat: add custom type definitions for Express to include user information in Locals

---
+++ src/@types/express/index.d.ts
@@ -0,0 +1,11 @@
+// src/@types/express/index.d.ts
+import "express";
+import { User } from "~/models/User";
+
+declare global {
+  namespace Express {
+    interface Locals {
+      user?: Pick<User, "id" | "role">;
+    }
+  }
+}
