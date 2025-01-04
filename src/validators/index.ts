1186ef1fb9162b46b27907df162eeb8acf93b102
Oky Aneka <okyaneka@gmail.com>
2025-01-04 08:37:09 +0700

    feat: update README and add docker-compose for MongoDB setup

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,4 +1,5 @@
 import Joi from "joi";
+import schema from "~/helpers/schema";
 
 export interface DefaultListParams {
   page: number;
@@ -10,4 +11,10 @@ export const DefaultListParamsFields = {
   page: Joi.number().default(1),
   limit: Joi.number().default(10),
   search: Joi.string().allow("").default(""),
+};
+
+export const DefaultListQueryFields = {
+  page: schema.number({ defaultValue: 1 }),
+  limit: schema.number({ defaultValue: 1 }),
+  search: schema.string({ defaultValue: "", allow: "" }),
 };
