b23b67b8984d84f1133ac0e85616d75c3ade3c5d
Oky Aneka <okyaneka@gmail.com>
2025-01-04 16:37:47 +0700

    feat: restructure routes for admin

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,10 @@
 import { Express } from "express";
 import AuthRoute from "./AuthRoute";
-import StageRoute from "./StageRoute";
-import QrRoute from "./QrRoute";
-import ChallengeRoute from "./ChallengeRoute";
+import admin from "./admin";
 
 const routes = (app: Express) => {
   app.use(AuthRoute);
-  app.use("/stage", StageRoute);
-  app.use("/challenge", ChallengeRoute);
-  app.use("/qr", QrRoute);
+  app.use("/admin", admin);
 };
 
 export default routes;
