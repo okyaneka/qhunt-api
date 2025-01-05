37cc1262988fcc0aefa072bb8eb956ebd1ca9a34
Oky Aneka <okyaneka@gmail.com>
2025-01-05 12:09:34 +0700

    feat: add QrRoute for verifying QR codes

---
+++ src/routes/QrRoute/index.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import response from "~/helpers/response";
+import { QrService } from "~/services";
+
+const path = {
+  verify: "/verify/:code",
+} as const;
+
+const QrRoute = Router();
+
+QrRoute.get(path.verify, async (req, res, next) => {
+  const code = req.params.code;
+
+  const item = await QrService.verify(code, res.locals.TID as string).catch(
+    (err: Error) => err
+  );
+
+  if (item instanceof Error) return next(item);
+
+  res.json(response.success(item));
+});
+
+export default QrRoute;
