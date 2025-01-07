61ace35d0c8c3d08b85d857bd791c6d150651c8a
Oky Aneka <okyaneka@gmail.com>
2025-01-07 18:51:53 +0700

    feat: enhance UserStage model with status and score fields

--- src/validators/UserStageValidator/index.ts
+++ src/validators/UserStageValidator/index.ts
@@ -1,8 +1,21 @@
 import schema from "~/helpers/schema";
-import { UserStageForeign } from "~/models/UserStage";
+import {
+  UserStageForeign,
+  UserStageListParams,
+  UserStageStatus,
+} from "~/models/UserStage";
+import { DefaultListParamsFields } from "..";
 
 export const UserStageForeignValidator = schema.generate<UserStageForeign>({
   id: schema.string({ required: true }),
   stageId: schema.string({ required: true }),
   name: schema.string({ required: true }),
 });
+
+export const UserStageListParamsValidator =
+  schema.generate<UserStageListParams>({
+    ...DefaultListParamsFields,
+    status: schema
+      .string({ allow: "" })
+      .valid(...Object.values(UserStageStatus)),
+  });
