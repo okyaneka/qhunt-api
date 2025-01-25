1131a75172c492c76c443d7b322f0f5dcf96e151
Oky Aneka <okyaneka@gmail.com>
2025-01-25 15:41:36 +0700

    refactor: remove debug log from CookiesMiddleware and update CORS origin regex

--- src/plugins/mongoose.ts
+++ src/plugins/mongoose.ts
@@ -4,8 +4,9 @@ import { env } from "~/configs";
 const mongoose = async () => {
   try {
     const uri = env.MONGO_URI;
-    await db.connect(uri, { dbName: env.MONGO_DB_NAME });
+    const connection = await db.connect(uri, { dbName: env.MONGO_DB_NAME });
     console.log("MongoDB connected successfully!");
+    return connection;
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
