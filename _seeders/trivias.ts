beed3f634ace53c751276bdb45728ab477ea8037
Oky Aneka <okyaneka@gmail.com>
2025-01-28 00:44:03 +0700

    fix: update trivia question points from 100 to 200

--- _seeders/trivias.ts
+++ _seeders/trivias.ts
@@ -120,7 +120,7 @@ const trivias: TriviaPayload[] = [
       "Tari Gong merupakan tarian khas suku Dayak yang berasal dari Kalimantan Timur. Tarian ini diabadikan dalam uang kertas. Pada nominal berapakah tarian ini?",
     options: [
       { isCorrect: false, point: 0, text: "10.000" },
-      { isCorrect: true, point: 100, text: "20.000" },
+      { isCorrect: true, point: 200, text: "20.000" },
       { isCorrect: false, point: 0, text: "50.000" },
       { isCorrect: false, point: 0, text: "5000" },
     ],
@@ -134,7 +134,7 @@ const trivias: TriviaPayload[] = [
       { isCorrect: false, point: 0, text: "Batam" },
       { isCorrect: false, point: 0, text: "Anambas" },
       { isCorrect: false, point: 0, text: "Karimun" },
-      { isCorrect: true, point: 100, text: "Bintan" },
+      { isCorrect: true, point: 200, text: "Bintan" },
     ],
     allowMultiple: false,
     feedback: { negative: "Aduh", positive: "Nice" },
@@ -144,7 +144,7 @@ const trivias: TriviaPayload[] = [
     options: [
       { isCorrect: false, point: 0, text: "DKI Jakarta" },
       { isCorrect: false, point: 0, text: "Jawa Barat" },
-      { isCorrect: true, point: 100, text: "Jawa Tengah" },
+      { isCorrect: true, point: 200, text: "Jawa Tengah" },
       { isCorrect: false, point: 0, text: "Jawa Timur" },
     ],
     allowMultiple: false,
@@ -156,7 +156,7 @@ const trivias: TriviaPayload[] = [
     options: [
       { isCorrect: false, point: 0, text: "Kalimantan Tengah" },
       { isCorrect: false, point: 0, text: "Sulawesi Tengah" },
-      { isCorrect: true, point: 100, text: "Papua Pegunungan" },
+      { isCorrect: true, point: 200, text: "Papua Pegunungan" },
       { isCorrect: false, point: 0, text: "Jambi" },
     ],
     allowMultiple: false,
@@ -167,7 +167,7 @@ const trivias: TriviaPayload[] = [
       "Dalam film Finding Nemo, ayah Nemo bertemu dengan sekelompok penyu. Siapakah nama ayah Nemo?",
     options: [
       { isCorrect: false, point: 0, text: "Crush" },
-      { isCorrect: true, point: 100, text: "Marlin" },
+      { isCorrect: true, point: 200, text: "Marlin" },
       { isCorrect: false, point: 0, text: "Squirt" },
       { isCorrect: false, point: 0, text: "Dori" },
     ],
@@ -178,7 +178,7 @@ const trivias: TriviaPayload[] = [
     question:
       "Manakah dari provinsi berikut yang memiliki luas laut palin luas?",
     options: [
-      { isCorrect: true, point: 100, text: "Maluku" },
+      { isCorrect: true, point: 200, text: "Maluku" },
       { isCorrect: false, point: 0, text: "Bangka Belitung" },
       { isCorrect: false, point: 0, text: "Sulawesi Utara" },
       { isCorrect: false, point: 0, text: "Kepulauan Riau" },
@@ -192,7 +192,7 @@ const trivias: TriviaPayload[] = [
     options: [
       { isCorrect: false, point: 0, text: "1990" },
       { isCorrect: false, point: 0, text: "1991" },
-      { isCorrect: true, point: 100, text: "1992" },
+      { isCorrect: true, point: 200, text: "1992" },
       { isCorrect: false, point: 0, text: "1993" },
     ],
     allowMultiple: false,
@@ -205,7 +205,7 @@ const trivias: TriviaPayload[] = [
       { isCorrect: false, point: 0, text: "Woody" },
       { isCorrect: false, point: 0, text: "Buzz" },
       { isCorrect: false, point: 0, text: "Jessie" },
-      { isCorrect: true, point: 100, text: "Bullseye" },
+      { isCorrect: true, point: 200, text: "Bullseye" },
     ],
     allowMultiple: false,
     feedback: { negative: "Aduh", positive: "Nice" },
@@ -214,7 +214,7 @@ const trivias: TriviaPayload[] = [
     question:
       "Selain Cut Nyak Dhien, siapakah pahlawan Aceh yang pernah diabadikan dalam uang rupiah?",
     options: [
-      { isCorrect: true, point: 100, text: "Cut Nyak Meutia" },
+      { isCorrect: true, point: 200, text: "Cut Nyak Meutia" },
       { isCorrect: false, point: 0, text: "Teuku Umar" },
       { isCorrect: false, point: 0, text: "Sultan Iskandar Muda" },
       { isCorrect: false, point: 0, text: "Sultan Mahmud Badaruddin II" },
@@ -226,7 +226,7 @@ const trivias: TriviaPayload[] = [
     question: "10+1x0(5x2)?",
     options: [
       { isCorrect: false, point: 0, text: "0" },
-      { isCorrect: true, point: 100, text: "10" },
+      { isCorrect: true, point: 200, text: "10" },
       { isCorrect: false, point: 0, text: "20" },
       { isCorrect: false, point: 0, text: "11" },
     ],
