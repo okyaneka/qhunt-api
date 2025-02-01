326630393a70c116fc18b547293c16b40b884c92
Oky Aneka <okyaneka@gmail.com>
2025-02-01 16:56:15 +0700

    feat: add photohunt feature for challenge + and refactor trivia item

---
+++ src/routes/admin/ChallengeRoute/PhotoHuntRoute/index.ts
@@ -0,0 +1,41 @@
+import { Request, Router } from "express";
+import { response } from "qhunt-lib/helpers";
+import { PhotoHuntService } from "qhunt-lib/services";
+import { ValidationMiddleware } from "~/middlewares";
+import { PhotoHuntPayloadValidator } from "qhunt-lib/validators/PhotoHuntValidator";
+
+type Params = {
+  id: string;
+};
+
+const path = { details: "/details", sync: "/sync" } as const;
+
+const PhotoHuntRoute = Router({ mergeParams: true });
+
+PhotoHuntRoute.get(path.details, async (req: Request<Params>, res, next) => {
+  const id = req.params.id;
+
+  const data = await PhotoHuntService.details(id).catch((err) => err);
+
+  if (data instanceof Error) return next(data);
+
+  res.json(response.success({ items: data, totalItem: data.length }));
+});
+
+PhotoHuntRoute.put(
+  path.sync,
+  ValidationMiddleware({ body: PhotoHuntPayloadValidator }),
+  async (req: Request<Params>, res, next) => {
+    const id = req.params.id;
+
+    const payload = await PhotoHuntPayloadValidator.validateAsync(req.body);
+
+    const data = await PhotoHuntService.sync(id, payload).catch((err) => err);
+
+    if (data instanceof Error) return next(data);
+
+    res.json(response.success({ items: data, totalItem: data.length }));
+  }
+);
+
+export default PhotoHuntRoute;
--- src/routes/admin/ChallengeRoute/index.ts
+++ src/routes/admin/ChallengeRoute/index.ts
@@ -1,72 +1,65 @@
 import { response } from "qhunt-lib/helpers";
 import { Router } from "express";
 import { AuthMiddleware, ValidationMiddleware } from "~/middlewares";
-import { ChallengeService, TriviaService } from "qhunt-lib/services";
+import { ChallengeService } from "qhunt-lib/services";
 import {
-  ChallengeType,
-  ChallengeTypeValues,
-} from "qhunt-lib/models/ChallengeModel";
-import { ChallengeListParamsValidator } from "qhunt-lib/validators/ChallengeValidator";
-import { TriviaItemsPayloadValidator } from "qhunt-lib/validators/TriviaValidator";
+  ChallengeListParamsValidator,
+  ChallengePayloadValidator,
+} from "qhunt-lib/validators/ChallengeValidator";
+import TriviaRoute from "./TriviaRoute";
+import PhotoHuntRoute from "./PhotoHuntRoute";
 
 const path = {
   list: "/list",
   create: "/create",
   detail: "/:id/detail",
-  detailContent: "/:id/detail/content",
   update: "/:id/update",
-  updateContent: "/:id/update/content",
   delete: "/:id/delete",
+  trivia: "/:id/trivia",
+  photoHunt: "/:id/photohunt",
 } as const;
 
 const ChallengeRoute = Router();
 
 ChallengeRoute.use(AuthMiddleware);
 
+ChallengeRoute.use(path.trivia, TriviaRoute);
+ChallengeRoute.use(path.photoHunt, PhotoHuntRoute);
+
 ChallengeRoute.get(
   path.list,
   ValidationMiddleware({ query: ChallengeListParamsValidator }),
-  async (req, res) => {
-    const { value: params } = ChallengeListParamsValidator.validate(req.query);
+  async (req, res, next) => {
+    const params = await ChallengeListParamsValidator.validateAsync(req.query);
 
-    const data = await ChallengeService.list(params);
+    const data = await ChallengeService.list(params).catch((err) => err);
+
+    if (data instanceof Error) return next(data);
 
     res.json(response.success(data));
   }
 );
 
-// ChallengeRoute.post(
-//   path.create,
-//   ValidationMiddleware({ body: ChallengePayloadValidator }),
-//   async (req, res) => {
-//     const { value } = ChallengePayloadValidator.validate(req.body);
+ChallengeRoute.post(
+  path.create,
+  ValidationMiddleware({ body: ChallengePayloadValidator }),
+  async (req, res) => {
+    const { value } = ChallengePayloadValidator.validate(req.body);
 
-//     const item = await ChallengeService.create(value).catch(
-//       (err: Error) => err
-//     );
+    const item = await ChallengeService.create(value).catch(
+      (err: Error) => err
+    );
 
-//     if (item instanceof Error) {
-//       res.status(400).json(response.error(item.message));
-//       return;
-//     }
+    if (item instanceof Error) {
+      res.status(400).json(response.error(item.message));
+      return;
+    }
 
-//     res.json(response.success(item));
-//   }
-// );
+    res.json(response.success(item));
+  }
+);
 
-// ChallengeRoute.get(path.detail, async (req, res) => {
-//   const id = req.params.id;
-
-//   const item = await ChallengeService.detail(id).catch((err: Error) => err);
-//   if (item instanceof Error) {
-//     res.status(400).json(response.error(item.message));
-//     return;
-//   }
-
-//   res.json(response.success(item));
-// });
-
-ChallengeRoute.get(path.detailContent, async (req, res) => {
+ChallengeRoute.get(path.detail, async (req, res) => {
   const id = req.params.id;
 
   const item = await ChallengeService.detail(id).catch((err: Error) => err);
@@ -75,66 +68,29 @@ ChallengeRoute.get(path.detailContent, async (req, res) => {
     return;
   }
 
-  switch (item.settings.type) {
-    case ChallengeTypeValues.Trivia:
-      const triviaContent = await TriviaService.content(item);
-      res.json(response.success(triviaContent));
-      return;
-    default:
-      res.json();
-      return;
-  }
+  res.json(response.success(item));
 });
 
-// ChallengeRoute.put(
-//   path.update,
-//   ValidationMiddleware({ body: ChallengePayloadValidator }),
-//   async (req, res) => {
-//     const { value } = ChallengePayloadValidator.validate(req.body);
+ChallengeRoute.put(
+  path.update,
+  ValidationMiddleware({ body: ChallengePayloadValidator }),
+  async (req, res) => {
+    const { value } = ChallengePayloadValidator.validate(req.body);
 
-//     const id = req.params.id;
+    const id = req.params.id;
 
-//     const item = await ChallengeService.update(id, value).catch(
-//       (err: Error) => err
-//     );
+    const item = await ChallengeService.update(id, value).catch(
+      (err: Error) => err
+    );
 
-//     if (item instanceof Error) {
-//       res.status(400).json(response.error(item.message));
-//       return;
-//     }
+    if (item instanceof Error) {
+      res.status(400).json(response.error(item.message));
+      return;
+    }
 
-//     res.json(response.success(item));
-//   }
-// );
-
-ChallengeRoute.put(path.updateContent, async (req, res, next) => {
-  const id = req.params.id;
-
-  const item = await ChallengeService.detail(id).catch((err: Error) => err);
-  if (item instanceof Error) return next(item);
-
-  switch (item.settings.type) {
-    case ChallengeTypeValues.Trivia:
-      const { value: triviaValue, error: triviaError } =
-        TriviaItemsPayloadValidator.validate(req.body, { abortEarly: false });
-
-      if (triviaError) {
-        res.status(400).json(response.errorValidation(triviaError));
-        return;
-      }
-
-      const trivias = await TriviaService.sync(item, triviaValue.items).catch(
-        (err: Error) => err
-      );
-      if (trivias instanceof Error) return next(trivias);
-      break;
-
-    default:
-      break;
+    res.json(response.success(item));
   }
-
-  res.json(response.success("content synced"));
-});
+);
 
 ChallengeRoute.delete(path.delete, async (req, res) => {
   const id = req.params.id;
