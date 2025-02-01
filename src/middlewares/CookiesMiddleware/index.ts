5a34010e30ef897eaa3ae3c41189f41d4d542ea5
Oky Aneka <okyaneka@gmail.com>
2025-02-01 15:03:54 +0700

    fix: handle invalid public authentication in CookiesMiddleware (#47)

--- src/middlewares/CookiesMiddleware/index.ts
+++ src/middlewares/CookiesMiddleware/index.ts
@@ -2,6 +2,7 @@ import cookies from "~/configs/cookies";
 import { RequestHandler } from "express";
 import { UserPublicService } from "qhunt-lib/services";
 import { UserPublic } from "qhunt-lib/models/UserPublicModel";
+import { response } from "qhunt-lib/helpers";
 
 const CookiesMiddleware: RequestHandler = async (req, res, next) => {
   const TID = req.cookies[cookies.TID];
@@ -14,6 +15,10 @@ const CookiesMiddleware: RequestHandler = async (req, res, next) => {
   }
 
   if (!user) user = await UserPublicService.setup();
+  if (!user) {
+    res.status(401).json(response.error({}, "invalid public auth", 403));
+    return;
+  }
 
   res.cookie(cookies.TID, user.code, cookies.options);
   res.locals.TID = user.code;
