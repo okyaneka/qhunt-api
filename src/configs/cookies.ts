5043dd90676c0ca1e33b4981d26e4fb7141d8a08
Oky Aneka <okyaneka@gmail.com>
2025-02-05 08:55:07 +0700

    fix: cookies config

--- src/configs/cookies.ts
+++ src/configs/cookies.ts
@@ -3,6 +3,10 @@ import { CookieOptions } from "express";
 const options: CookieOptions = {
   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1e3),
   path: "/",
+  sameSite: "none",
+  secure: true,
+  domain: ".kulooky.my.id",
+  httpOnly: true,
 };
 
 const cookies = {
