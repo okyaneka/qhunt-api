92ac0c00e574de3a5a4d1b39c7da7dad09a89955
Oky Aneka <okyaneka@gmail.com>
2025-02-24 21:59:56 +0700

    feat: enhance handler to return success response and improve type handling

--- _seeders/photohunts.ts
+++ _seeders/photohunts.ts
@@ -2,6 +2,15 @@ import { PhotoHuntPayload } from "qhunt-lib";
 
 const photohunts: PhotoHuntPayload[] = [
   { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
+  { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
+  { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
+  { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
+  { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
+  { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
+  { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
+  { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
+  { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
+  { hint: "Cari aja sampai ketemu!", feedback: "Keren", score: 100 },
 ];
 
 export default photohunts;
