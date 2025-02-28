5d9065cece2a3152b1db2333e1884679952a3a28
Oky Aneka <okyaneka@gmail.com>
2025-03-01 01:06:21 +0700

    refactor: streamline authentication and cookie handling in middlewares (#62)

--- src/middlewares/AuthMiddleware/index.ts
+++ src/middlewares/AuthMiddleware/index.ts
@@ -9,32 +9,21 @@ const AuthMiddleware: RequestHandler = async (
   res: Response,
   next: NextFunction
 ) => {
-  // const bearer = req.headers.authorization;
-  // if (!bearer || !bearer.startsWith("Bearer ")) {
-  //   res.status(401).json(response.error({}, "unauthorized", 401));
-  //   return;
-  // }
+  Promise.resolve()
+    .then(async () => {
+      const token = req.cookies[cookies.TOKEN] as string;
 
-  const token = req.cookies[cookies.TOKEN] as string;
+      verify(token, env.JWT_SECRET);
 
-  verify(token, env.JWT_SECRET, (err) => {
-    if (err) {
-      res.status(401).json(response.error({}, err.message, 401));
-      return;
-    }
-  });
+      const { id } = decode(token) as { id: string };
+      const user = await UserService.detail(id);
 
-  const { id } = decode(token) as { id: string };
-  const user = await UserService.detail(id).catch((err: Error) => err);
-  if (!user || user instanceof Error) {
-    res.status(401).json(response.error({}, "invalid auth", 401));
-    return;
-  }
+      res.locals.TID = user.meta.code;
+      res.locals.user = { id: user.id, role: user.role };
 
-  res.locals.TID = user.meta.code;
-  res.locals.user = { id: user.id, role: user.role };
-
-  next();
+      next();
+    })
+    .catch(next);
 };
 
 export default AuthMiddleware;
